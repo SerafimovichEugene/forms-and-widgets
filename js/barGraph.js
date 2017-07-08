9a58bac67bfe89072e74239248bce3f43a377626
SerafimovichEugene <eugene.serafimovich@gmail.com>
2017-07-08 20:16:42 +0300

    feature: implement email widget

--- js/barGraph.js
+++ js/barGraph.js
@@ -3,14 +3,12 @@ function setDayOfWeek(days) {
     columns.forEach((column) => {
         for (var property in days) {
             if (days.hasOwnProperty(property)) {
-                
                 if(column.children[0].className === ('inner-' + property)) {
                     let height = 207*(days[property] / 100);
                     let realValue = 60*(days[property] / 100);
                     column.firstElementChild.style.height = height;
                     column.lastElementChild.style.marginBottom = height;
                     column.lastElementChild.firstElementChild.innerHTML = Math.floor(realValue);
-                    
                 }
             }
         }        
@@ -39,6 +37,7 @@ let days = {
     sat: 66,
     sun: 36
 }
+
 window.addEventListener("load", function () {
     showColumnValue();
     setTimeout(function () {
